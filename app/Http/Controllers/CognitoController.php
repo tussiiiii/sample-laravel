0b7a59498298c0432071e0cf5a17b903599d6f1e
ikumi.tsushima <ikumi.tsushima@dowell.co.jp>
2022-01-07 09:41:44 +0900

    ユーザー情報取得処理の修正

--- app/Http/Controllers/CognitoController.php
+++ app/Http/Controllers/CognitoController.php
@@ -63,10 +63,10 @@ class CognitoController extends Controller
       
         // step 3: Get the payload
         $result = JWT::decode($encoded_jwt, $pub_key, ['ES256']);
-        // // var_dump($result);
+        var_dump($result);
 
         // return redirect('/')->with($decoded_json);
-        return redirect('/')->with($result);
+        // return redirect('/')->with($result);
       }
 
 }
